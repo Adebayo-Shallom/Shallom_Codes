bba3160c3543e23b1fe9a7522f0977e8016766df
Adebayo-Shallom <112850479+Adebayo-Shallom@users.noreply.github.com>
2023-09-12 14:02:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home(slack_name:str = "Pamode", track:str = "backend"):
         "current_day": current_day,
         "utc_time": utc_time,
         "track": track,
-        "github_file_url": "https://github.com/Adebayo-Shallom/shallomcodes.github.io/blob/main/endpoint/endpoint/app.py",
+        "github_file_url": "https://github.com/Adebayo-Shallom/Shallom_Codes/blob/7144bc14b2f5e18995f78f823178855c80d7c798/app.py",
         "github_repo_url": "https://github.com/Adebayo-Shallom/shallomcodes.github.io",
         "status_code": 200
     }
